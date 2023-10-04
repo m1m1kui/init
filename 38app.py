a7afeb3826c6df85b17d52aba8684d6263256d4e
m1m1kui <rgg.rhh@yandex.ru>
2023-10-04 19:35:31 +0700

    init2

--- 38app.py
+++ 38app.py
@@ -20,9 +20,11 @@ while running:
     rect = rect.move(speed)
     if rect.left < 0 or rect.right > width:
         speed[0] = -speed[0]
+        print('dodo')
 
     if rect.top < 0 or rect.bottom > height:
         speed[1] = -speed[1]
+        print('fppfpg')
 
     screen.fill((255, 255, 255))
     pygame.draw.rect(screen, RED, rect, 1)
